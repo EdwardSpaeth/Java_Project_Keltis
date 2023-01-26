181b374c0eb31d2d3520dd3d83ff6c55e0c887cc
VaseeST <vasee.vst@gmail.com>
2023-01-26 15:11:39 +0100

    Rulescontroller and rules.fxml combined  App.java

--- src/main/java/com/KeltisT/Controllers/rulesController.java
+++ src/main/java/com/KeltisT/Controllers/rulesController.java
@@ -1,12 +1,17 @@
 package com.KeltisT.Controllers;
 
-import javafx.scene.Group;
-import javafx.scene.layout.StackPane;
+import com.KeltisT.Window.SizeOfMonitor;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,6 +19,12 @@ import java.nio.file.Path;
 
 //still figuring out how to fix the rules text
 public class rulesController {
+    private SizeOfMonitor sizeOfMonitor = new SizeOfMonitor();
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    private double HEIGHT = sizeOfMonitor.getSizeOfMonitor()[0];
+    private double WIDTH = sizeOfMonitor.getSizeOfMonitor()[1];
 
     public rulesController() throws IOException {
         Path filename  = Path.of("src/main/resources/Rules.txt");
@@ -24,5 +35,15 @@ public class rulesController {
         text.setStrokeWidth(0.5);
         text.setFont(Font.font("Papyrus", FontWeight.NORMAL, FontPosture.REGULAR, 12));
 
+    }
+    // Back Button
+    public void switchToStart(ActionEvent event) throws IOException {
+
+        root = FXMLLoader.load(getClass().getResource("/Fxml/start.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, WIDTH, HEIGHT);
+        System.out.println(WIDTH + " and " + HEIGHT);
+        stage.setScene(scene);
+        stage.show();
     }
 }
