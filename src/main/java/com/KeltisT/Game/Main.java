3becc8dcf551e8240a78db1097b5e6de0422c934
Edward PC <Edward5000@gmx.de>
2023-02-03 00:15:27 +0100

    deleted some useless code?

--- src/main/java/com/KeltisT/Game/Main.java
+++ src/main/java/com/KeltisT/Game/Main.java
@@ -14,9 +14,6 @@ public class Main {
     // DOES NOT WORK YET
     public static void start_game(GameEngine gameEngine) {
 
-        //System.out.println("Main has started!");
-
-        //PhysicalChip pchip = gameengine.get_gameboard().get_chips().get(0);
         ArrayList<String> color_names = new ArrayList<>(Arrays.asList("brown", "yellow", "pink", "green", "blue"));
         for (PhysicalChip pchip : gameEngine.get_gameboard().get_chips()) {
             EventHandler<MouseEvent> myhandler;
@@ -28,22 +25,11 @@ public class Main {
             });
             pchip.get_text().setOnMouseClicked(myhandler);
         }
-
-        /*
-        for (int x = 0; x < 10; x++) {
-        // Change it to line below WHEN it works
-        //while (!gameengine.check_if_game_over()) {
-            //Boolean clover_was_played = PLAYER_TURN(gameengine.curr_player);
-            Boolean clover_was_played = Boolean.FALSE;
-            gameengine.next_turn(clover_was_played);
-        }
-        */
     }
 
     private static void chip_has_been_selected(PhysicalChip pChip, GameEngine gameEngine) {
         if (pChip.get_is_hidden()) {
             pChip.uncover();
-            //System.out.println("Player " + gameEngine.get_curr_player().get_name() + " has uncovered chip with color=" + color_names.get(pChip.get_color()) + " and value=" + pChip.get_value());
             take_chip_or_not(gameEngine, pChip);
         } else {
             // TRANSFER CHIP
@@ -57,13 +43,6 @@ public class Main {
     //Boolean take_chip_boolean = new Boolean(Boolean.FALSE);
     private static void take_chip_or_not(GameEngine gameEngine, PhysicalChip pchip) {
         gameEngine.get_gameboard().make_blocker_visible(true);
-        /*
-        Button take = new Button("Take Chip");
-        take.setLayoutX(500);
-        take.setLayoutY(0);
-        gameController GameController = new gameController();
-        GameController.showButtons();
-        */
         gameEngine.get_takeButton().setVisible(true);
         gameEngine.get_leaveButton().setVisible(true);
         if (gameEngine.get_curr_player().get_stacks().get(pchip.get_color()).check_if_insert_possible(pchip) == Boolean.FALSE) {
@@ -72,20 +51,12 @@ public class Main {
         else {
             gameEngine.get_takeButton().setDisable(false);
         }
-        /*
-        Button leave = new Button("Leave Chip");
-        leave.setLayoutX(500);
-        leave.setLayoutY(100);
-        */
         gameEngine.get_takeButton().setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 gameEngine.get_takeButton().setVisible(Boolean.FALSE);
                 gameEngine.get_leaveButton().setVisible(Boolean.FALSE);
                 gameEngine.get_gameboard().make_blocker_visible(false);
-                gameController GameController = new gameController();
-                //GameController.hideButtons();
-                //System.out.println("Player wants to take chip");
                 gameEngine.get_curr_player().get_stacks().get(pchip.get_color()).insert(gameEngine.get_gameboard().transfer_chip_ownership(pchip));
                 pchip.remove();
                 gameEngine.get_curr_player().update_points();
@@ -101,9 +72,6 @@ public class Main {
                 gameEngine.get_takeButton().setVisible(Boolean.FALSE);
                 gameEngine.get_leaveButton().setVisible(Boolean.FALSE);
                 gameEngine.get_gameboard().make_blocker_visible(false);
-                gameController GameController = new gameController();
-                //GameController.hideButtons();
-                //System.out.println("Player does not want to take chip");
                 // If you are just uncovering a chip, you cannot get its clover bonus. Therefore argument is FALSE
                 gameEngine.next_turn(Boolean.FALSE);
                 if(gameEngine.check_if_game_over()) {
@@ -111,8 +79,6 @@ public class Main {
                 }
             }
         });
-        //gameEngine.get_gameboard().get_gameboard_chips_group().getChildren().addAll(take, leave);
-
     }
 
     private static void game_over(GameEngine gameEngine) {
