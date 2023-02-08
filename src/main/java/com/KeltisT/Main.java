0d262073443a0d504f565c78d140ce53c239dd42
VaseeST <vasee.vst@gmail.com>
2023-02-08 20:57:27 +0100

    Slider Functions and saving Audio Settings

--- src/main/java/com/KeltisT/Main.java
+++ src/main/java/com/KeltisT/Main.java
@@ -1,7 +1,7 @@
 package com.KeltisT;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         App.main(args);
     }
 }
